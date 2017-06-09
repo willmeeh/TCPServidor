3962622d03e71712753387f7c1d30054daf5ce71
m96627 <m96627@PC0822.acad.unisc.br>
2017-06-08 21:26:36 -0300

    Criado um pacote que contem as dependencias do projeto, e dicionado biblioteca para ler JSON nesta pasta

--- src/tcpserver/Comandos.java
+++ src/tcpserver/Comandos.java
@@ -18,8 +18,6 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import sun.misc.IOUtils;
 
